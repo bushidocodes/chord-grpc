0c11bbe9a21f788b88fcef92b2d40b9aed56ea56
Alvaro Albero <ealbero94@gmail.com>
2019-10-25 16:58:28 -0400

    Update driver with the launch sequence of nodes

--- driver.ps1
+++ driver.ps1
@@ -1,4 +1,6 @@
-node node --ip localhost --port 8448 --id 0 --targetIp localhost --targetPort 8448 --targetId 0
-node node --ip localhost --port 8449 --id 1 --targetIp localhost --targetPort 8448 --targetId 0
-node node --ip localhost --port 8450 --id 2 --targetIp localhost --targetPort 8448 --targetId 0
-node node --ip localhost --port 8451 --id 3 --targetIp localhost --targetPort 8448 --targetId 0
+node node --ip localhost --port 8440 --id 0 --targetIp localhost --targetPort 8440 --targetId 0
+node node --ip localhost --port 8441 --id 6 --targetIp localhost --targetPort 8440 --targetId 0
+node node --ip localhost --port 8442 --id 1 --targetIp localhost --targetPort 8440 --targetId 0
+node node --ip localhost --port 8443 --id 2 --targetIp localhost --targetPort 8440 --targetId 0
+node node --ip localhost --port 8445 --id 3 --targetIp localhost --targetPort 8440 --targetId 0
+node node --ip localhost --port 8444 --id 7 --targetIp localhost --targetPort 8440 --targetId 0
