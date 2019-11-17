9b9fab3a92a9d8ec5b0f483f632dcf90ecb5afb2
heidiklum <43496381+heidiklum@users.noreply.github.com>
2019-11-17 05:26:22 +0000

    replaced ip with host

--- client.js
+++ client.js
@@ -3,12 +3,14 @@ const path = require("path");
 const caller = require("grpc-caller");
 const PROTO_PATH = path.resolve(__dirname, "./protos/chord.proto");
 
-const HOST = "127.0.0.1";
+const CRAWLER_INTERVAL_MS = 1000;
+const DEFAULT_HOST_NAME = "localhost";
+const DEFAULT_HOST_PORT = 1337;
 const DUMMY_REQUEST_OBJECT = { id: 99 };
 
 const target = {
-  ip: HOST,
-  port: 1337
+  host: DEFAULT_HOST_NAME,
+  port: DEFAULT_HOST_PORT
 };
 
 let client;
@@ -99,7 +101,7 @@ async function summary() {
   try {
     const node = await client.summary({ id: 1 });
     console.log(
-      `The node returned id: ${node.id}, ip: ${node.ip}, port: ${node.port}`
+      `The node returned id: ${node.id}, host: ${node.host}, port: ${node.port}`
     );
   } catch (err) {
     console.error(err);
@@ -107,8 +109,8 @@ async function summary() {
 }
 
 class ChordCrawler {
-  constructor(ip, port, stepInMS) {
-    this.ip = ip;
+  constructor(host, port, stepInMS) {
+    this.host = host;
     this.port = port;
     this.state = {};
     this.walk = new Set([]);
@@ -138,7 +140,7 @@ class ChordCrawler {
     }
   }
   updateNode(node) {
-    const connectionString = `${node.ip}:${node.port}`;
+    const connectionString = `${node.host}:${node.port}`;
     this.state[connectionString] = {
       ...this.state[connectionString],
       ...node
@@ -148,7 +150,9 @@ class ChordCrawler {
     // If we have trouble reaching a node, just shuffle to any other node and walk from there
     const otherNodes = Object.values(this.state).filter(
       node =>
-        (node.ip !== this.ip || node.port !== this.port) && this.ip && this.port
+        (node.host !== this.host || node.port !== this.port) &&
+        this.host &&
+        this.port
     );
 
     // Just return if we don't have any possible alternatives
@@ -159,14 +163,14 @@ class ChordCrawler {
     const randomNode =
       otherNodes[Math.floor(Math.random() * otherNodes.length)];
 
-    this.ip = randomNode.ip;
+    this.host = randomNode.host;
     this.port = randomNode.port;
 
     // And we have to invalidate the current walk to avoid accidental pruning
     this.walk.clear();
   }
   async crawl() {
-    const connectionString = `${this.ip}:${this.port}`;
+    const connectionString = `${this.host}:${this.port}`;
     console.log(`Connecting to ${connectionString}`);
     const client = caller(connectionString, PROTO_PATH, "Node");
 
@@ -177,7 +181,7 @@ class ChordCrawler {
       this.pruneUponCycle(connectionString);
       this.updateSuccessor(connectionString, successorNode);
       this.updateNode(successorNode);
-      this.ip = successorNode.ip;
+      this.host = successorNode.host;
       this.port = successorNode.port;
     } catch (err) {
       console.error("Error is : ", err);
@@ -190,18 +194,18 @@ function main() {
   if (process.argv.length >= 3) {
     const args = minimist(process.argv.slice(3));
 
-    console.log(args);
+    console.log("The command-line arguments were:\n", args);
 
-    if (args.ip) {
-      target.ip = args.ip;
+    if (args.host) {
+      target.host = args.host;
     }
     if (args.port) {
       target.port = args.port;
     }
 
-    console.log(`Connecting to ${target.ip}:${target.port}`);
+    console.log(`Connecting to ${target.host}:${target.port}`);
 
-    client = caller(`localhost:${target.port}`, PROTO_PATH, "Node");
+    client = caller(`${target.host}:${target.port}`, PROTO_PATH, "Node");
 
     const command = process.argv[2];
 
@@ -224,10 +228,15 @@ function main() {
         summary();
         break;
       case "crawl":
-        let crawler = new ChordCrawler(target.ip, target.port, 3000);
+        let crawler = new ChordCrawler(
+          target.host,
+          target.port,
+          CRAWLER_INTERVAL_MS
+        );
         const express = require("express");
         const app = express();
-        const port = args.webPort || 3000;
+        // TBD is this OR a bug?
+        const port = args.webPort || CRAWLER_INTERVAL_MS;
         app.use(express.static("public"));
         app.get("/data", (req, res) => res.json(crawler.state));
         app.listen(port, () =>
--- commands.md
+++ commands.md
@@ -1,28 +1,28 @@
 # Manual without Docker
 
-npm run devServer -- --ip localhost --port 8440 --id 0 --targetIp localhost --targetPort 8440 --targetId 0
-npm run devClient -- crawl --ip localhost --port 8440 --webPort 1337
+npm run devServer -- --host localhost --port 8440 --knownHost localhost --knownPort 8440
+npm run devClient -- crawl --host localhost --port 8440 --webPort 1337
 
-npm run devServer -- --ip localhost --port 8441 --id 6 --targetIp localhost --targetPort 8440 --targetId 0
-npm run devServer -- --ip localhost --port 8442 --id 1 --targetIp localhost --targetPort 8440 --targetId 0
-npm run devServer -- --ip localhost --port 8443 --id 2 --targetIp localhost --targetPort 8440 --targetId 0
-npm run devServer -- --ip localhost --port 8445 --id 3 --targetIp localhost --targetPort 8440 --targetId 0
-npm run devServer -- --ip localhost --port 8444 --id 7 --targetIp localhost --targetPort 8440 --targetId 0
+npm run devServer -- --host localhost --port 8441 --knownHost localhost --knownPort 8440
+npm run devServer -- --host localhost --port 8444 --knownHost localhost --knownPort 8440
+npm run devServer -- --host localhost --port 8446 --knownHost localhost --knownPort 8440
+npm run devServer -- --host localhost --port 8448 --knownHost localhost --knownPort 8440
+npm run devServer -- --host localhost --port 8450 --knownHost localhost --knownPort 8440
 
 <!-- Alvaro: npm run devClient worked for me, but it does not ends so I can't run the following command -->
 
-node client insert --ip localhost --port 8440 --id 2
-node client insert --ip localhost --port 8440 --id 5 --displayName "Alvaro is cool" --reputation 99 --aboutMe "I'm so cool I need no description"
-node client lookup --ip localhost --port 8440 --id 2
-node client remove --ip localhost --port 8440 --id 2
+node client insert --host localhost --port 8440
+node client insert --host localhost --port 8440 --displayName "Alvaro is cool" --reputation 99 --aboutMe "I'm so cool I need no description"
+node client lookup --host localhost --port 8440
+node client remove --host localhost --port 8440
 
 # Manual with Docker
 
-I can't get the containers to communicate with each other. No idea about what IP should be used
+I can't get the containers to communicate with each other. No idea about what IP or host name should be used
 
 docker build -t bushidocodes/chord -f ./NodeDockerfile .
 docker build -t bushidocodes/chordweb -f ./ClientDockerfile .
 
-docker run -p 8440:1337 -it --init bushidocodes/chord --id 0 --targetIp host.docker.internal --targetPort 8440 --targetId 0
-docker run -p 8441:1337 -it --init bushidocodes/chord --id 6 --targetIp host.docker.internal --targetPort 8440 --targetId 0
-docker run -p 1337:1337 -it --init bushidocodes/chordweb crawl --ip host.docker.internal --port 8440 --webPort 1337
+docker run -p 8440:1337 -it --init bushidocodes/chord --knownHost host.docker.internal --knownPort 8440
+docker run -p 8441:1337 -it --init bushidocodes/chord --knownHost host.docker.internal --knownPort 8440
+docker run -p 1337:1337 -it --init bushidocodes/chordweb crawl --host host.docker.internal --port 8440 --webPort 1337
